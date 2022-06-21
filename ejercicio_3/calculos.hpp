60e38a0c19c32bb190f0ebaef41fd5a495f48a10
Joaquina <joaquipisani@gmail.com>
2022-06-21 13:17:35 -0300

    agrego el main de prueba y ademas modifiaciones del hpp

---
+++ ejercicio_3/calculos.hpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+typedef enum {
+    ACUMULAR,
+    PROMEDIAR
+} _state;
+
+uint16_t sgn[64] = {
+#include "../ejercio_2/sgn.h"
+};
+
+
+class calculos {
+public:
+explicit calculos(){}
+~calculos(){}
+
+void init () {
+    estado=ACUMULAR;
+    counter=0;
+    acum=0;
+    //N=rand()%(63 + 1);
+    N=2;
+}
+
+void fms_calculadora(){
+switch(estado){
+    case ACUMULAR:
+    while(counter < N)
+    {
+      acum=acum+sgn[counter];
+      counter++;
+    }
+    estado=PROMEDIAR;
+
+    break;
+    case PROMEDIAR:
+    avg=acum/N;
+    cout<<avg<<endl;
+    acum=0;
+    counter=0;
+    estado=ACUMULAR;
+    break;
+    
+}
+}
+
+private:
+uint32_t N;
+uint32_t counter;
+uint32_t acum;
+uint32_t avg;
+_state estado;
+
+};
