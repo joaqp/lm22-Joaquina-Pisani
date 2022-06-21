4d19d5b2545afe2b05f1cd48745a9f899f851749
Joaquina <joaquipisani@gmail.com>
2022-06-21 10:56:48 -0300

    Ejercicio 2 genero la rampa

---
+++ ejercicio_2/ramp.py
@@ -0,0 +1,14 @@
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+
+y = np.random.randint(1, 3300, 64)
+y=np.sort(y)
+    
+print(list(y))
+plt.stem(y, y)
+plt.xlabel('  n--->  ')
+plt.ylabel('Amplitude ----> ')
+plt.title('Unit ramp Signal')
+plt.grid(True)
+plt.show()
